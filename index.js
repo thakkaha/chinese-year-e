e4d4687a9d14874215b81fb3e636b22803f416f2
happy हर्षः <32341763+thakkaha@users.noreply.github.com>
2026-01-13 13:48:11 -0500

    v1

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-const years = [
-  'Rat 🐀',
-  'Ox 🐂',
-  'Tiger 🐅',
-  'Rabbit 🐇',
-  'Dragon 🐉',
-  'Snake 🐍',
-  'Horse 🐎',
-  'Sheep 🐏',
-  'Monkey 🐒',
-  'Rooster 🐓',
-  'Dog 🐕',
-  'Pig 🐖'
-]
+const years = ['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Sheep', 'Monkey', 'Rooster', 'Dog', 'Pig'];
+const yearse=['&#x1F400;', '&#x1F402;','&#x1F405;','&#x1F407;','&#x1F409;','&#x1F40D;','&#x1F40E;','&#x1F411;','&#x1F412;','&#x1F413;','&#x1F415;','&#x1F416;'];
 
 exports.years = years
 
 exports.getAnimal = num => {
-  const ind = num % 12
-  let name 
-  if (ind >3) {
-    name = years[ind-4]
-  } else {
-    name = years[ind + 8]
-  }
-  if (!name) throw new Error(`Opps, error`)
+  const ind = num % 12;
+  let name =years[ind];
+  name+= ' '+yearse[ind];
+    if (!name) throw new Error(`Opps, error`)
   return name
 }
